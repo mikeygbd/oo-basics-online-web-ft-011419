09a8f76169d696fae89e6b9b2a27528710fef872
Michael Soares <mikey.soares@gmail.com>
2019-01-21 16:13:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,9 +11,9 @@ class Shoe
   def cobble
   if condition = "old"
     condition = "New"
-      return "Your shoe is as good as new!"
+      puts "Your shoe is as good as new!"
     else
-      return "Your shoe is as good as new!"
+      puts "Your shoe is as good as new!"
     end
   end
 
