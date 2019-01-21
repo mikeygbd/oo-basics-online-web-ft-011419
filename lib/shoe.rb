26db671dd317f04fb8ff972240a31871958d1fca
Michael Soares <mikey.soares@gmail.com>
2019-01-21 16:10:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,18 @@
 
 class Shoe
   attr_reader :brand
-  attr_accessor :color, :size, :material, :condition, :cobble
+  attr_accessor :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
+
+  def cobble
+    condition = new
+    if condition == new
+
+      return "Your shoe is as good as new!"
+    end
 
 
 end
